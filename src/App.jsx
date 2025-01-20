73ad30ee523fafeebe1717401726702055904393
Carlos Pacheco Aviles <carlospachecoaviles@192.168.68.106>
2025-01-19 20:46:35 -0600

    Initial commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,20 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Us from "./pages/Us";
 import Developer from "./pages/Developer";
 import Marketing from "./pages/Marketing";
 import Footer from "./components/Footer";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Us />,
-  },
-  {
-    path: "/Website",
-    element: <Developer />,
-  },
-  {
-    path: "/Marketing",
-    element: <Marketing />,
-  },
-]);
-
 const App = () => {
   return (
     <>
       <Header />
       <div className="overflow-hidden ">
-        <RouterProvider router={router} />;
+        <Routes>
+          <Route path="/" element={<Us />} />
+          <Route path="/Website" element={<Developer />} />
+          <Route path="/Marketing" element={<Marketing />} />
+        </Routes>
       </div>
       <div className="align-self-end">
         <Footer />
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,21 @@
-import { StrictMode } from "react";
+import  ReactDOM  from  "react-dom/client" ; 
 import { createRoot } from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import { BrowserRouter, createHashRouter, HashRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Us from "./pages/Us.jsx";
 import Marketing from "./pages/Marketing.jsx";
 import Developer from "./pages/Developer.jsx";
 
-const router = createHashRouter([
-  {
-    path: "/",
-    element: <Us />,
-  },
-  {
-    path: "/Website",
-    element: <Developer />,
-  },
-  {
-    path: "/Marketing",
-    element: <Marketing />,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <Router>
       <NextUIProvider>
         <main>
           <App />
         </main>
       </NextUIProvider>
-    </HashRouter>
+    </Router>
   </StrictMode>
 );
