b9479ac76d22368c04209f0c0a0150483fcca9e8
Carlos Pacheco Aviles <carlospachecoaviles@192.168.68.106>
2025-01-19 19:17:30 -0600

    base update 2

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://INCODE.github.io/incodeweb/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
